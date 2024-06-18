332d875c7963059120b5a08fbea79d2ac3187927
DipakKumar6-code <126050058+DipakKumar6-code@users.noreply.github.com>
2024-06-18 07:47:30 -0400

    I have added LabWebApp project that will demonstrate the basic concept of mvc.

---
+++ LabWebApp/LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using LabWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ LabWebApp/LabWebApp/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LabWebApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+

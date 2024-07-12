6c2d67ca1ef90f21da804a578d00dfbff7e2010a
DipakKumar6-code <126050058+DipakKumar6-code@users.noreply.github.com>
2024-07-12 03:33:18 -0400

    I have added Signin with GitHub as a third party authentication

--- LabWebApp/LabWebApp/Program.cs
+++ LabWebApp/LabWebApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using LabWebApp.Data;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,22 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddAuthentication()
+    .AddGitHub(o =>
+    {
+        o.ClientId = builder.Configuration["Authentication:GitHub:ClientId"];
+        o.ClientSecret = builder.Configuration["Authentication:GitHub:ClientSecret"];
+        o.CallbackPath = "/signin-github";
+        o.Scope.Add("read:user");
+        o.Events.OnCreatingTicket = context =>
+        {
+            if (context.AccessToken is { })
+            {
+                context.Identity?.AddClaim(new Claim("access_token", context.AccessToken));
+            }
+            return Task.CompletedTask;
+        };
+    });
 
 builder.Services.AddControllersWithViews();
 
---
+++ LabWebApp/LabWebApp/Views/Shared/_LoginPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LabWebApp.Views.Shared
+{
+    public class _LoginPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
